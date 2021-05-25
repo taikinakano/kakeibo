2d4ce9ae7896426bb47c27e83acc6949b1ce5dd4
taikinakano <kabayaki3323@gmail.com>
2021-05-25 14:51:15 +0000

    [Update]new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,22 @@
 class BooksController < ApplicationController
-    
+
     def index
         @books = Book.all
     end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+        @book.year = 2019
+    end
     
+    def create
+        book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+        @book = Book.new(book_params)
+        @book.save
+        redirect_to books_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/books", to: "books#index"
+  post "/books", to: "books#create"
+  get "/books/new", to: "books#new", as: "new_book"
+  get "/books/:id", to: "books#show",as: "book"
 end
