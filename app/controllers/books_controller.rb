efe4e878ee67a132caeb164d710ee3bbb381e806
taikinakano <kabayaki3323@gmail.com>
2021-05-28 06:49:16 +0000

    [Updat]partial implementation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
+ before_action :set_book, only: [:show,:edit,:update,:destroy]
 
     def index
         @books = Book.all
     end
 
     def show
-        @book = Book.find(params[:id])
+
     end
 
     def new
@@ -26,11 +27,11 @@ class BooksController < ApplicationController
     end
 
     def edit
-        @book = Book.find(params[:id])
+
     end
 
     def update
-        @book = Book.find(params[:id])
+
         book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
         if @book.update(book_params)
           flash[:notice]="データを1件更新しました"
@@ -42,9 +43,15 @@ class BooksController < ApplicationController
     end
 
     def destroy
-        @book = Book.find(params[:id])
+
         @book.destroy
         flash[:notice] = "削除しました。"
         redirect_to books_path
+    end
+
+    private
+
+    def set_book
+        @book = Book.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/books", to: "books#index"
-  post "/books", to: "books#create"
-  get "/books/new", to: "books#new", as: "new_book"
-  get "/books/:id/edit", to: "books#edit", as: "edit_book"
-  get "/books/:id", to: "books#show",as: "book"
-  patch "/books/:id", to: "books#update"
-  delete "/books/:id", to: "books#destroy"
+ resources :books
 end
